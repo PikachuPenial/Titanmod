a2cdae3520589c996b95b56b5235516b827868e3
PortableTerraria <63077153+PortableTerraria@users.noreply.github.com>
2022-09-24 22:59:06 -0500

    lazy penial
    lazy bitch ass bitch

---
+++ gamemodes/titanmod/gamemode/cl_killhud.lua
@@ -0,0 +1,7 @@
+// HI PENILE!
+
+net.Receive( "NotifyKill", function( len, ply ) // this'll be recieved by the server's killnotification.lua
+
+    local notificationString = net.ReadString() // reads the string sent by server
+
+end )
