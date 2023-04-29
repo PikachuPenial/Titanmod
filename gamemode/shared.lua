c48a60c896986d19241670d3700f2a14bfe51cc9
Penial <62630906+PikachuPenial@users.noreply.github.com>
2023-04-29 17:42:39 -0500

    Removed weapon-attached flashlights
    cool

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -107,7 +107,7 @@ end)
 
 --Disable UBGLs
 hook.Add("TFABase_ShouldLoadAttachment", "DisableUBGL", function(id, path)
-    if id and (id == "ins2_fg_gp25" or id == "ins2_fg_m203" or id == "r6s_flashhider_2" or id == "r6s_h_barrel" or id == "am_gib") then
+    if id and (id == "ins2_fg_gp25" or id == "ins2_fg_m203" or id == "r6s_flashhider_2" or id == "r6s_h_barrel" or id == "am_gib" or id == "flashlight" or id == "flashlight_lastac" or id == "ins2_eft_lastac2" or id == "tfa_at_fml_flashlight" or id == "un_flashlight" or id == "ins2_ub_flashlight") then
         return false
     end
 end)
