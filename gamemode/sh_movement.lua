d17b9986527bd02a483ad2007dcc5af22733366f
Penial <62630906+PikachuPenial@users.noreply.github.com>
2023-11-18 01:04:15 -0600

    Wall jumping prediction
    W

--- gamemode/sh_movement.lua
+++ gamemode/sh_movement.lua
@@ -21,10 +21,10 @@ function meta:SetWJTime(value)
 end
 
 function meta:GetWRTime()
-    return self:GetDTFloat(20)
+    return self:GetDTFloat(22)
 end
 function meta:SetWRTime(value)
-    return self:SetDTFloat(20, value)
+    return self:SetDTFloat(22, value)
 end
 
 local qslide_duration = CreateConVar("sv_qslide_duration", 0.75, {FCVAR_REPLICATED, FCVAR_ARCHIVE})
@@ -113,65 +113,113 @@ hook.Add("Move", "TM_Move", function(ply, mv)
 
     if ply:GetMoveType() == MOVETYPE_LADDER then return end
 
-    -- WR Left
-    if goingLeft and not goingRight and sprinting and not onground then
-        if ply:GetWRTime() > CT then return end
+    if goingLeft then
+        -- WJ Left
+        if jumping then
+            if ply:GetWJTime() > CT then return end
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Up() * -1) + (ang:Forward() * 32)
-        tracedata.endpos = eyepos + (ang:Up() * -53) + (ang:Forward() * 64)
-        tracedata.filter = ply
-        local traceWalLLow = util.TraceLine(tracedata)
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Right() * 1)
+            tracedata.endpos = eyepos + (ang:Right() * 32)
+            tracedata.filter = ply
+            local traceWallLeft = util.TraceLine(tracedata)
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Forward() * 32)
-        tracedata.endpos = eyepos + (ang:Forward() * 64)
-        tracedata.filter = ply
-        local traceWalLHigh = util.TraceLine(tracedata)
+            if (traceWallLeft.Hit) then
+                ply:SetWJTime(CT + wallJumpTime)
+                ply:ViewPunch(walljumpleftpunch)
+                if SERVER then ply:EmitSound("player/footsteps/tile" .. math.random(1, 4) .. ".wav", 70, 80) end
+                vel = Vector(0, 0, 280) + ((ang:Right() * -1) * 175)
+                mv:SetVelocity(vel)
+                mv:SetOrigin(pos)
+            end
+        end
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Right() * -1)
-        tracedata.endpos = eyepos + (ang:Right() * -32)
-        tracedata.filter = ply
-        local traceWalLRight = util.TraceLine(tracedata)
+        --WR Left
+        if sprinting and not onground then
+            if ply:GetWRTime() > CT then return end
 
-        if not traceWalLLow.Hit and not traceWalLHigh.Hit and traceWalLRight.Hit then
-            ang.x = 0 ang.z = 0
-            ply:SetWRTime(CT + wallRunTime)
-            ply:ViewPunch(wallrunleftpunch)
-            if SERVER then ply:EmitSound("wallrun.wav") end
-            vel = Vector(0, 0, 210) + (ang:Right() * -200) + (ang:Forward() * 360)
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Up() * -1) + (ang:Forward() * 32)
+            tracedata.endpos = eyepos + (ang:Up() * -53) + (ang:Forward() * 64)
+            tracedata.filter = ply
+            local traceWalLLow = util.TraceLine(tracedata)
+
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Forward() * 32)
+            tracedata.endpos = eyepos + (ang:Forward() * 64)
+            tracedata.filter = ply
+            local traceWalLHigh = util.TraceLine(tracedata)
+
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Right() * -1)
+            tracedata.endpos = eyepos + (ang:Right() * -32)
+            tracedata.filter = ply
+            local traceWalLRight = util.TraceLine(tracedata)
+
+            if not traceWalLLow.Hit and not traceWalLHigh.Hit and traceWalLRight.Hit then
+                ang.x = 0 ang.z = 0
+                ply:SetWRTime(CT + wallRunTime)
+                ply:ViewPunch(wallrunleftpunch)
+                if SERVER then ply:EmitSound("wallrun.wav") end
+                vel = Vector(0, 0, 210) + (ang:Right() * -200) + (ang:Forward() * 360)
+                mv:SetVelocity(vel)
+                mv:SetOrigin(pos)
+            end
         end
     end
 
-    -- WR Right
-    if goingRight and not goingLeft and sprinting and not onground then
-        if ply:GetWRTime() > CT then return end
+    if goingRight then
+        -- WJ Right
+        if jumping then
+            if ply:GetWJTime() > CT then return end
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Up() * -1) + (ang:Forward() * 32)
-        tracedata.endpos = eyepos + (ang:Up() * -53) + (ang:Forward() * 64)
-        tracedata.filter = ply
-        local traceWalRLow = util.TraceLine(tracedata)
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Right() * -1)
+            tracedata.endpos = eyepos + (ang:Right() * -32)
+            tracedata.filter = ply
+            local traceWallRight = util.TraceLine(tracedata)
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Forward() * 32)
-        tracedata.endpos = eyepos + (ang:Forward() * 64)
-        tracedata.filter = ply
-        local traceWalRHigh = util.TraceLine(tracedata)
+            if (traceWallRight.Hit) then
+                ply:SetWJTime(CT + wallJumpTime)
+                ply:ViewPunch(walljumprightpunch)
+                if SERVER then ply:EmitSound("player/footsteps/tile" .. math.random(1, 4) .. ".wav", 70, 80) end
+                vel = Vector(0, 0, 280) + ((ang:Right() * 1) * 175)
+                mv:SetVelocity(vel)
+                mv:SetOrigin(pos)
+            end
+        end
 
-        tracedata = {}
-        tracedata.start = eyepos + (ang:Right() * 1)
-        tracedata.endpos = eyepos + (ang:Right() * 32)
-        tracedata.filter = ply
-        local traceWalRRight = util.TraceLine(tracedata)
+        -- WR Right
+        if sprinting and not onground then
+            if ply:GetWRTime() > CT then return end
 
-        if not traceWalRLow.Hit and not traceWalRHigh.Hit and traceWalRRight.Hit then
-            ang.x = 0 ang.z = 0
-            ply:SetWRTime(CT + wallRunTime)
-            ply:ViewPunch(wallrunrightpunch)
-            if SERVER then ply:EmitSound("wallrun.wav") end
-            vel = Vector(0, 0, 210) + (ang:Right() * 200) + (ang:Forward() * 360)
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Up() * -1) + (ang:Forward() * 32)
+            tracedata.endpos = eyepos + (ang:Up() * -53) + (ang:Forward() * 64)
+            tracedata.filter = ply
+            local traceWalRLow = util.TraceLine(tracedata)
+
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Forward() * 32)
+            tracedata.endpos = eyepos + (ang:Forward() * 64)
+            tracedata.filter = ply
+            local traceWalRHigh = util.TraceLine(tracedata)
+
+            tracedata = {}
+            tracedata.start = eyepos + (ang:Right() * 1)
+            tracedata.endpos = eyepos + (ang:Right() * 32)
+            tracedata.filter = ply
+            local traceWalRRight = util.TraceLine(tracedata)
+
+            if not traceWalRLow.Hit and not traceWalRHigh.Hit and traceWalRRight.Hit then
+                ang.x = 0 ang.z = 0
+                ply:SetWRTime(CT + wallRunTime)
+                ply:ViewPunch(wallrunrightpunch)
+                if SERVER then ply:EmitSound("wallrun.wav") end
+                vel = Vector(0, 0, 210) + (ang:Right() * 200) + (ang:Forward() * 360)
+                mv:SetVelocity(vel)
+                mv:SetOrigin(pos)
+            end
         end
     end
 
@@ -179,9 +227,6 @@ hook.Add("Move", "TM_Move", function(ply, mv)
         ply:SetWJTime(CT)
         ply:SetWRTime(CT)
     end
-
-    mv:SetVelocity(vel)
-    mv:SetOrigin(pos)
 end)
 
 hook.Add("FinishMove", "TM_FinishMove", function(ply, mv)
